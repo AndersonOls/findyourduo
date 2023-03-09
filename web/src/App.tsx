77a3cc68d8fd871a428fe891f7a12a63185b764d
Anderson Silva <anderson.silva1996@hotmail.com>
2023-03-09 20:22:12 +0000

    tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
-import './styles.css'
+import './styles/main.css'
 
 
 function App() {
-  return 
-  <div>
-  </div>
+  return(
+    <div className='w-8 h-8 bg-violet-500'>
+      <p>teste</p>
+    </div>
+  )
 }
 
 export default App
